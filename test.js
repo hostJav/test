1a5828a7a722330815bc29927128dc44e9e70a6d
hostJav <31518249+hostJav@users.noreply.github.com>
2017-09-01 12:01:54 +0200

    Update test.js

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ function load_js()
       var head= document.getElementsByTagName('head')[0];
       var script= document.createElement('script');
       script.type= 'text/javascript';
-      script.src= 'https://cdn.rawgit.com/hostJav/test/c0f5977a/test.js';
+      script.src= 'https://rawgit.com/hostJav/test/master/test.js';
       head.appendChild(script);      
    }
 function block() {
