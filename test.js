b96f0e0fac5c678756d2364158672752f5adfc6f
hostJav <31518249+hostJav@users.noreply.github.com>
2017-09-01 11:23:12 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,10 @@
 console.log(window.location.hostname);
+function load_js()
+   {
+      var head= document.getElementsByTagName('head')[0];
+      var script= document.createElement('script');
+      script.type= 'text/javascript';
+      script.src= 'https://raw.githubusercontent.com/hostJav/test/master/test.js';
+      head.appendChild(script);
+   }
+   load_js();
