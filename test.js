dd222a2ed0aa49fd2e85e4c780b1e7c0a8fa4d7e
hostJav <31518249+hostJav@users.noreply.github.com>
2017-10-05 13:27:46 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,17 +1,4 @@
-function load_js()
-   {
-      var head= document.getElementsByTagName('head')[0];
-      var script= document.createElement('script');
-      script.type= 'text/javascript';
-      script.src= 'https://rawgit.com/hostJav/test/master/test.js';
-      head.appendChild(script);      
-   }
-function block() {
-    var siteName = window.location.hostname;
-    var blockSiteList = ["sportowefakty.wp.pl", "onet.pl", "pl-pl.facebook.com"];     
-    if (siteName.indexOf("pl-pl.facebook.com") !== -1 || siteName.indexOf("sportowefakty.wp.pl") !== -1 || siteName.indexOf("onet.pl") !== -1)
-        window.location.replace("http://stackoverflow.com");
-}
+
 block();
 
 
