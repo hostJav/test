8d0aa93ea52ba3ba68834c6ec4f7546551a88b70
hostJav <31518249+hostJav@users.noreply.github.com>
2017-10-05 13:39:31 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
-
-alert("alert")
-
-
-
+function block() {
+      var siteName = window.location.hostname;     
+     if (siteName.indexOf("pl-pl.facebook.com") !== -1 || siteName.indexOf("sportowefakty.wp.pl") !== -1 || siteName.indexOf("onet.pl") !== -1)
+          window.location.replace("https://stackoverflow.com");
+  }
+ block();
